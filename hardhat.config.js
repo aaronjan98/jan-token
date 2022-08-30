84df25c96f1802b165f4a08ef5ea76f405f90f0c
aaronjan98 <aaronjan98@gmail.com>
2022-08-29 18:47:32 -0700

    using simple hard task for smoke test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+
+// This is a sample Hardhat task.
+// https://hardhat.org/hardhat-runner/docs/advanced/create-task
+task("accounts", "Prints  the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+})
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
