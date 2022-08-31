16ee91ba80345c3f730a67d2f19fede7079d436b
aaronjan98 <aaronjan98@gmail.com>
2022-08-31 16:02:07 -0700

    create first test to confirm token name fxn

--- test/Token.js
+++ test/Token.js
@@ -6,8 +6,9 @@ describe('Token', () => {
     it('has a name', async () => {
         // Fetch Token from Blockchain
         const Token = await ethers.getContractFactory('Token')
+        let token = await Token.deploy()
         // Read token name
-        const name = await Token.name()
+        const name = await token.name()
         // check that name is correct
         expect(name).to.equal('Jan Token')
     })
