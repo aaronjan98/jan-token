9b4c7259fda6584c4fa7bf9738b3ec38a8e58779
aaronjan98 <aaronjan98@gmail.com>
2022-09-13 15:59:50 -0700

    test for Trade

--- test/Exchange.js
+++ test/Exchange.js
@@ -285,6 +285,21 @@ describe('Exchange', () => {
                 expect(await exchange.balanceOf(token2.address, user2.address)).to.equal(tokens(0.9))
                 expect(await exchange.balanceOf(token2.address, feeAccount.address)).to.equal(tokens(0.1))
             })
+
+            it('emits a Trade event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Trade')
+
+                const args = event.args
+                expect(args.id).to.equal(1)
+                expect(args.user).to.equal(user2.address)
+                expect(args.tokenGet).to.equal(token2.address)
+                expect(args.amountGet).to.equal(tokens(1))
+                expect(args.tokenGive).to.equal(token1.address)
+                expect(args.amountGive).to.equal(tokens(1))
+                expect(args.creator).to.equal(user1.address)
+                expect(args.timestamp).to.at.least(1)
+            })
         })
     })
 })
