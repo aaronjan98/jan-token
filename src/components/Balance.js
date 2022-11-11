043c941b7ca5102f1d6cbde87bdd1b293862bab0
aaronjan98 <aaronjan98@gmail.com>
2022-11-11 15:01:25 -0800

    implement withdrawals from exchange

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -75,6 +75,32 @@ const Balance = () => {
         }
     }
 
+    const withdrawHandler = (e, token) => {
+        e.preventDefault()
+
+        if (token.address === tokens[0].address) {
+            transferTokens(
+                provider,
+                exchange,
+                'Withdraw',
+                token,
+                token1TransferAmount,
+                dispatch
+            )
+            setToken1TransferAmount(0)
+        } else {
+            transferTokens(
+                provider,
+                exchange,
+                'Withdraw',
+                token,
+                token2TransferAmount,
+                dispatch
+            )
+            setToken2TransferAmount(0)
+        }
+    }
+
     useEffect(() => {
         if (exchange && tokens[0] && tokens[1] && account) {
             loadBalances(exchange, tokens, account, dispatch)
@@ -125,7 +151,13 @@ const Balance = () => {
                     </p>
                 </div>
 
-                <form onSubmit={e => depositHandler(e, tokens[0])}>
+                <form
+                    onSubmit={
+                        isDeposit
+                            ? e => depositHandler(e, tokens[0])
+                            : e => withdrawHandler(e, tokens[0])
+                    }
+                >
                     <label htmlFor="token0">
                         {symbols && symbols[0]} Amount
                     </label>
@@ -175,7 +207,13 @@ const Balance = () => {
                     </p>
                 </div>
 
-                <form onSubmit={e => depositHandler(e, tokens[1])}>
+                <form
+                    onSubmit={
+                        isDeposit
+                            ? e => depositHandler(e, tokens[1])
+                            : e => withdrawHandler(e, tokens[1])
+                    }
+                >
                     <label htmlFor="token1"></label>
                     <input
                         type="text"
